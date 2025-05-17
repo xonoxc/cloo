4c83218f0d0ea373c9c89eca994ddb60c37655a9
arpit <arpit2003y@gmail.com>
2025-05-17 09:34:25 +0530

    chore(init):initalized_project

--- lib/icons/Info.tsx
+++ lib/icons/Info.tsx
@@ -1,4 +1,5 @@
-import { Info } from 'lucide-react-native';
-import { iconWithClassName } from './iconWithClassName';
-iconWithClassName(Info);
-export { Info };
+import { Info } from "lucide-react-native"
+import { iconWithClassName } from "./iconWithClassName"
+iconWithClassName(Info)
+export { Info }
+
